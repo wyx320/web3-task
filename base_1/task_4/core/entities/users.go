75db0855a677fdf3f390f89fd60e997dc1a3000d
wyx320 <1090430801@qq.com>
2025-06-02 01:29:49 +0800

    task4_unfinish

--- base_1/task_4/core/entities/users.go
+++ base_1/task_4/core/entities/users.go
@@ -5,6 +5,7 @@ type UserEntity struct {
 	Id       uint64 `gorm:"primary_key;autoIncrement"`
 	Username string
 	Password string
+	Salt     string
 	Email    string
 }
 
---
+++ base_1/task_4/http_api/models/user/user_register_dto.go
@@ -0,0 +1,7 @@
+package user_register_dto
+
+type UserRegisterDto struct {
+	Username string
+	Password string
+	Email    string
+}

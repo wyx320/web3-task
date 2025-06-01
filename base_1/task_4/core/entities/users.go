b24dfd7018893e95cf6f76f1df94cd52373b7b74
wyx320 <1090430801@qq.com>
2025-06-01 17:17:42 +0800

    task4_unfinish

---
+++ base_1/task_4/core/entities/users.go
@@ -0,0 +1,13 @@
+package entities
+
+// 用户实体
+type UserEntity struct {
+	Id       uint64 `gorm:"primary_key;autoIncrement"`
+	Username string
+	Password string
+	Email    string
+}
+
+func (UserEntity) TableName() string {
+	return "users"
+}
---
+++ base_1/task_4/http_api/controllers/user_controller.go
@@ -0,0 +1,7 @@
+package user_controller
+
+import "github.com/gin-gonic/gin"
+
+func (ctrl *UserController) Registe(c *gin.Context) {
+
+}
